c06ca26740886ccdfbdef84ee3d2f89ab563cd80
Денис Димінський <denis.if.dyminskyy@gmail.com>
2023-02-09 16:01:18 +0200

    Test 1

--- OceanHunt/Program.cs
+++ OceanHunt/Program.cs
@@ -10,13 +10,7 @@ namespace OceanHunt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Updated by Den12321321");
-            Console.WriteLine("test");
-            Console.WriteLine("test");
-            Console.WriteLine("123123123");
-            Console.WriteLine("SDSDS");
-
-
+            Console.WriteLine("Hello! 1 ");
         }
     }
 }
