89afc8fa668b5760cccf8e500e9213b81a28e8e5
Денис Димінський <denis.if.dyminskyy@gmail.com>
2023-02-12 13:34:52 +0200

    Aaaa

--- OceanHunt/Ocean.cs
+++ OceanHunt/Ocean.cs
@@ -5,9 +5,29 @@ using System.Text;
 
 namespace OceanHunt
 {
-    
-
-    public class Ocean
+    public static class Ocean
     {
+        static int numRows = 25;            // число рядків
+        static int numCols = 70;            // число стовпців
+        static int size = numCols * numRows;
+        static int numPrey = 150;          // жертви
+        static int numPredators = 20;      // хижаки
+        static int numObstacles = 75;      // перешкоди
+
+        //public static int Run(int iterationCounter)
+        //{
+
+        //}
+        public static void PrintInfo()
+        {
+            Console.WriteLine("Enter number of rows: {0}", numRows);
+            Console.WriteLine("Enter number of columns:  {0}", numCols);
+            Console.WriteLine("Enter number of preys: {0}", numPrey);
+            Console.WriteLine("Enter number of predators: {0}", numPredators);
+            Console.WriteLine("Enter number of obstacles: {0}", numObstacles);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
     }
 }
